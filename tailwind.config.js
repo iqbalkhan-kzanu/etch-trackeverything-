15a4ed3c2723c72ba761aea32ea3e9af30983cd9
iqbalkhan-kzanu <userg5049@gmail.com>
2026-08-18 18:27:54 +0530

    jio

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,17 @@ export default {
         sans: ['"IBM Plex Sans"', 'sans-serif'],
         mono: ['"IBM Plex Mono"', 'monospace'],
       },
+      keyframes: {
+        blob: {
+          '0%, 100%': { transform: 'translate(0px, 0px) scale(1)' },
+          '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+          '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+        },
+      },
+      animation: {
+        blob: 'blob 14s infinite ease-in-out',
+      },
     },
   },
   plugins: [],
-}       
+}   
